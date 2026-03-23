2593bda7349ad430c588d5bd259b9bc58cc0f9de
kanha patidar <kanhapatidar7251@gmail.com>
2026-03-23 16:21:00 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ import os
 load_dotenv()
 
 EMAIL = os.getenv("EMAIL")
-PASSWORD = os.getenv("PASSWORD")
+PASSWORD = os.getenv("APP_PASSWORD")
 
 def send_email(receiver_email, otp):
     print("Connecting to Gmail...")
